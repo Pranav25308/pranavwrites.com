9c17dd9d871c74c73762028a3c9b2f6a8ccd4ac0
Pranav Yadav <pranavyadav700@gmail.com>
2026-06-23 11:09:15 +0530

    - Not sure what this commit is about, it was very long time ago. a Please enter the commit message for your changes. Lines starting

---
+++ app/lib/models/user.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, models, model } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin"],
+      default: "admin",
+    },
+  },
+  { timestamps: true }
+);
+
+export const User =
+  models.User || model("User", UserSchema);
